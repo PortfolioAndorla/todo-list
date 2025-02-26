e0226cd3c4b72b18b78d0f6544bdbbdf058a4d44
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-02-26 16:58:23 -0500

    feat: add header component

---
+++ src/app/public/components/header-content/header-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-content',
+  imports: [],
+  templateUrl: './header-content.component.html',
+  standalone: true,
+  styleUrl: './header-content.component.css'
+})
+export class HeaderContentComponent {
+
+}
