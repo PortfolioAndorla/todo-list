f9da30282bce0ce5f71f4c53ff435cb5da06990a
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-02-26 15:10:20 -0500

    chore: set prime ng config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+//prime ng config
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+
+  ]
 };
