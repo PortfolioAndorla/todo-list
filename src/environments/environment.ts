86f01782fdf7b510a8cbcc79ee46714f159d0736
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-02-27 23:28:14 -0500

    chore: set environment variables config

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // Server Base Path for TO-DO LIST REST API
+  //serverBasePath: 'http://localhost:3000/api/v1'
+  // Server Base Path for Spring Boot REST API
+  serverBasePath: 'http://localhost:8090/api/v1'
+};
