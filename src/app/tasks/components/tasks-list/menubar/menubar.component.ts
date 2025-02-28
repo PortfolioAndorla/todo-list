dcaf88c8a3aa202a81c1fd2531872de21461b029
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-02-27 23:29:17 -0500

    feat: add menubar component

--- src/app/tasks/components/tasks-list/menubar/menubar.component.ts
+++ src/app/tasks/components/tasks-list/menubar/menubar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { ToolbarModule } from 'primeng/toolbar';
 import {IconField} from 'primeng/iconfield';
@@ -7,35 +7,43 @@ import {SplitButton} from 'primeng/splitbutton';
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import {MenuItem} from 'primeng/api';
+import {Checkbox} from 'primeng/checkbox';
 
 @Component({
   selector: 'app-menubar',
   imports: [
     ToolbarModule,
     ButtonModule,
-    SplitButton,
     InputTextModule,
-    IconField,
-    InputIcon,
+    Checkbox,
 
   ],
   templateUrl: './menubar.component.html',
   standalone: true,
   styleUrl: './menubar.component.css'
 })
-export class MenubarComponent implements OnInit {
+export class MenubarComponent {
+  @Output() openFormDialog = new EventEmitter<any>();
+  @Output() checkAll = new EventEmitter<boolean>();
+  @Output() deleteAllTasks = new EventEmitter<void>();
+
+  checked: boolean = false;
   items: MenuItem[] | undefined;
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh'
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-times'
-      }
-    ];
+
+  deleteTasks(): void {
+    this.deleteAllTasks.emit();
   }
+
+  onCheckboxChange(): void {
+    this.checked = !this.checked;
+    this.checkAll.emit(this.checked);
+  }
+
+  formDialog() {
+    this.openFormDialog.emit();
+  }
+
+
+
 }
